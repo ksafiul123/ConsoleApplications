27c1b8ee19711f2f87803a39903caea7f2517d9f
ksafiul123 <ksafiul123@gmail.com>
2025-11-21 06:46:22 +0600

    interface file added

---
+++ src/InheritanceCheck.java
@@ -0,0 +1,24 @@
+class A{
+    public A() {
+        System.out.println("A");
+    }
+}
+
+class B extends A{
+    public B() {
+        System.out.println("B");
+    }
+}
+
+class C extends B{
+    public C() {
+        System.out.println("C");
+    }
+}
+
+public class InheritanceCheck {
+    public static void main(String[] args) {
+        C c = new C();
+    }
+
+}
